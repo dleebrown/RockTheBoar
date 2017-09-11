7e38fec1f28b5cb857baf365242f0f58cc9c9a43
Nesar Ramachandra <nayaknesar@gmail.com>
2017-09-11 00:38:32 -0500

    encoding added

--- keras_implementation/rtb_train.py
+++ keras_implementation/rtb_train.py
@@ -128,3 +128,32 @@ plotMask(x, y_pred)
 
 
 
+#-----------------------------------------------------------------------
+#Encoding
+
+def rle_encode(mask_image):
+    pixels = mask_image.flatten()
+    # We avoid issues with '1' at the start or end (at the corners of 
+    # the original image) by setting those pixels to '0' explicitly.
+    # We do not expect these to be non-zero for an accurate mask, 
+    # so this should not harm the score.
+    pixels[0] = 0
+    pixels[-1] = 0
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 2
+    runs[1::2] = runs[1::2] - runs[:-1:2]
+    return runs
+
+def rle_to_string(runs):
+    return ' '.join(str(x) for x in runs)
+
+#-----------------------------------------------------------------------
+
+
+rle = rle_encode(y_pred)
+
+rle_string = rle_to_string(rle)
+
+print rle_string
+
+
+
