18bf7367694be119e1585b446a0dac7c632f983a
dleebrown <donald@ku.edu>
2017-09-18 19:09:29 -0500

    added csv output

--- tensorflow_gan/input_pipeline.py
+++ tensorflow_gan/input_pipeline.py
@@ -6,11 +6,11 @@ import os
 import random
 
 # set these
-#car_image_directory = '/home/donald/Desktop/PYTHON/kaggle_car_competition/train/'
-#mask_image_directory = '/home/donald/Desktop/PYTHON/kaggle_car_competition/train_masks/'
+car_image_directory = '/home/donald/Desktop/PYTHON/kaggle_car_competition/train/'
+mask_image_directory = '/home/donald/Desktop/PYTHON/kaggle_car_competition/train_masks/'
 
-car_image_directory = '/home/nes/Desktop/Caravana/input/train/'
-mask_image_directory = '/home/nes/Desktop/Caravana/input/train_masks/'
+#car_image_directory = '/home/nes/Desktop/Caravana/input/train/'
+#mask_image_directory = '/home/nes/Desktop/Caravana/input/train_masks/'
 
 def downsample_img(input_img, scale_factor):
     img_size = input_img.size
@@ -49,16 +49,19 @@ def random_image_reader(list_of_images, total_num_images, scale_factor):
     root_name = chosen_image.split('.')[0]
     mask_name = root_name + '_mask.gif'
     pixelvals, mask = image_input(car_image_directory + chosen_image, mask_image_directory + mask_name, scale_factor)
-    print(root_name+'.jpg, ', end='') # added to match submission format'
+    # print(root_name+'.jpg, ', end='') # added to match submission format'
     #print(root_name+'.jpg, ',end="",flush=True) # added to match submission format'
     return pixelvals, mask
 
-
-# fetches the first num_fetch images in the directory
-def nonrandom_image_reader(list_of_images, total_num_images, scale_factor, num_fetch):
-    chosen_images = list_of_images[0:num_fetch]
-    root_names = [chosen_images[i].split('.')[0] for i in range(num_fetch)]
-    mask_names = [root_names[i] + '_mask.gif' for i in range(num_fetch)]
+# non-random image reader - basically the same as above but takes in an index of the image to read
+def not_random_image_reader(list_of_images, total_num_images, scale_factor, whichimage):
+    chosen_image = whichimage
+    chosen_image = list_of_images[chosen_image]
+    root_name = chosen_image.split('.')[0]
+    mask_name = root_name + '_mask.gif'
+    pixelvals, mask = image_input(car_image_directory + chosen_image, mask_image_directory + mask_name, scale_factor)
+    im_name = root_name+'.jpg'
+    return pixelvals, mask, im_name
 
 if __name__ == '__main__':
 
